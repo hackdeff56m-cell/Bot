bcb3f4afbfa9c6f23d031ec8f278bc3e6d919335
hackdeff56m-cell <hackdeff56m@gmail.com>
2026-04-27 18:52:20 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,34 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
+// teste de vida
 app.get("/", (req, res) => {
   res.send("Bot Minecraft ativo");
 });
 
-const port = process.env.PORT || 3000;
+// rota de comando (base do seu sistema TikTok)
+app.post("/comando", (req, res) => {
+  const msg = req.body.msg;
+
+  if (!msg) {
+    return res.status(400).send("sem comando");
+  }
+
+  if (msg === "zombie") {
+    return res.send("spawn zombie");
+  }
+
+  if (msg === "rosa") {
+    return res.send("spawn rose effect");
+  }
+
+  return res.send("comando desconhecido");
+});
+
+const port = process.env.PORT || 10000;
+
 app.listen(port, () => {
   console.log("Bot rodando na porta " + port);
 });
